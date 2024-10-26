1a03b63cc9a4f3603f39c4d16d53f875e963dbca
Tamanna@5092 <tamannaaktermoni51@gmailcom>
2024-10-27 00:47:15 +0600

    basic project setup

---
+++ src/layouts/Main.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Main = () => {
+    return (
+        <div>
+            {/* navbar */}
+            {/* outlet */}
+
+            {/* footer */}
+        </div>
+    );
+};
+
+export default Main;
